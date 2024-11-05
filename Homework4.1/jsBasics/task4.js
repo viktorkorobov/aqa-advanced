126346a4210f69fcd4686cf9aa514c2886c0be15
viktorkorobov <40566605+viktorkorobov@users.noreply.github.com>
2024-11-05 20:34:53 +0200

    Homework4.1-1

---
+++ Homework4.1/jsBasics/task4.js
@@ -0,0 +1,16 @@
+const radius = 5;
+const circleArea = Math.PI * Math.pow(radius, 2);
+
+console.log("Circle Area:", circleArea.toFixed(2));
+
+const length = 10;
+const width = 4;
+const rectangleArea = length * width;
+
+console.log("Rectangle Area:", rectangleArea.toFixed(2));
+
+const cylinderRadius = 7;
+const height = 10;
+const cylinderVolume = Math.PI * Math.pow(cylinderRadius, 2) * height;
+
+console.log("Cylinder Volume:", cylinderVolume.toFixed(2));
