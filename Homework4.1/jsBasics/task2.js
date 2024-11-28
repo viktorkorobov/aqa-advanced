5a93a8f74ce91a7245cb6e6f4f6b231eef082638
viktorkorobov <40566605+viktorkorobov@users.noreply.github.com>
2024-11-28 14:44:28 +0200

    Delete Homework4.1 directory

--- Homework4.1/jsBasics/task2.js
+++
@@ -1,8 +0,0 @@
-let person1= `Viktor`;
-let person2= `Bob`;
-
-let greetingConcat = "Hello, " + person1 + " and " + person2 + "!";
-console.log("Concatenation:", greetingConcat);
-
-let greetingTemplate = `Hello, ${person1} and ${person2}!`;
-console.log("Template String:", greetingTemplate);
